06ad5ee3099ff93c7baa0b72d63bfa7c044f191a
Ivan Zakrevsky <ivzak@yandex.ru>
2026-02-02 04:00:49 +0300

    Refactor CausalDependency

--- asceticddd/seedwork/domain/aggregate/causal_dependency.go
+++ asceticddd/seedwork/domain/aggregate/causal_dependency.go
@@ -1,69 +1,88 @@
 package aggregate
 
 func NewCausalDependency(
-	aggregateId any,
-	aggregateType string,
-	aggregateVersion uint,
+	tenantId any,
+	streamType string,
+	streamId any,
+	streamPosition uint,
 ) (CausalDependency, error) {
+	/*
+	   This is enough to extract aggregate with required version from repository.
+	   And this is enough to check causal dependencies in Inbox.
+	*/
 	return CausalDependency{
-		aggregateId:      aggregateId,
-		aggregateType:    aggregateType,
-		aggregateVersion: aggregateVersion,
+		tenantId:       tenantId,
+		streamType:     streamType,
+		streamId:       streamId,
+		streamPosition: streamPosition,
 	}, nil
 }
 
 type CausalDependency struct {
-	aggregateId      any
-	aggregateType    string
-	aggregateVersion uint
+	tenantId       any    // aggregate.id.tenant_id
+	streamType     string // bounded_context_name.aggregate_name
+	streamId       any    // aggregate.id.internal_id
+	streamPosition uint   // aggregate.version
 }
 
-func (d CausalDependency) AggregateId() any {
-	return d.aggregateId
+func (d CausalDependency) TenantId() any {
+	return d.tenantId
 }
 
-func (d CausalDependency) AggregateType() string {
-	return d.aggregateType
+func (d CausalDependency) StreamId() any {
+	return d.streamId
 }
 
-func (d CausalDependency) AggregateVersion() uint {
-	return d.aggregateVersion
+func (d CausalDependency) StreamType() string {
+	return d.streamType
+}
+
+func (d CausalDependency) StreamPosition() uint {
+	return d.streamPosition
 }
 
 func (d CausalDependency) Export(ex CausalDependencyExporterSetter) {
-	ex.SetAggregateId(d.aggregateId)
-	ex.SetAggregateType(d.aggregateType)
-	ex.SetAggregateVersion(d.aggregateVersion)
+	ex.SetStreamId(d.tenantId)
+	ex.SetStreamType(d.streamType)
+	ex.SetStreamId(d.streamId)
+	ex.SetStreamPosition(d.streamPosition)
 }
 
 type CausalDependencyExporterSetter interface {
-	SetAggregateId(any)
-	SetAggregateType(string)
-	SetAggregateVersion(uint)
+	SetTenantId(any)
+	SetStreamType(string)
+	SetStreamId(any)
+	SetStreamPosition(uint)
 }
 
 type CausalDependencyExporter struct {
-	AggregateId      any
-	AggregateType    string
-	AggregateVersion uint
+	TenantId       any
+	StreamType     string
+	StreamId       any
+	StreamPosition uint
 }
 
-func (ex *CausalDependencyExporter) SetAggregateId(val any) {
-	ex.AggregateId = val
+func (ex *CausalDependencyExporter) SetTenantId(val any) {
+	ex.TenantId = val
 }
-func (ex *CausalDependencyExporter) SetAggregateType(val string) {
-	ex.AggregateType = val
+
+func (ex *CausalDependencyExporter) SetStreamId(val any) {
+	ex.StreamId = val
 }
-func (ex *CausalDependencyExporter) SetAggregateVersion(val uint) {
-	ex.AggregateVersion = val
+func (ex *CausalDependencyExporter) SetStreamType(val string) {
+	ex.StreamType = val
+}
+func (ex *CausalDependencyExporter) SetStreamPosition(val uint) {
+	ex.StreamPosition = val
 }
 
 type CausalDependencyReconstitutor struct {
-	AggregateId      any
-	AggregateType    string
-	AggregateVersion uint
+	TenantId       any
+	StreamType     string
+	StreamId       any
+	StreamPosition uint
 }
 
 func (r CausalDependencyReconstitutor) Reconstitute() (CausalDependency, error) {
-	return NewCausalDependency(r.AggregateId, r.AggregateType, r.AggregateVersion)
+	return NewCausalDependency(r.TenantId, r.StreamType, r.StreamId, r.StreamPosition)
 }
